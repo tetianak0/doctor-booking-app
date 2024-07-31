8b661bbeec96bbbe70102f9bc14cccbfbb4ac612
JuliaPashkova <jjstudio1cn@gmail.com>
2024-07-31 16:00:52 +0200

    Update UserDetail.java

--- src/main/java/doctorBookingApp/security/UserDetail.java
+++ src/main/java/doctorBookingApp/security/UserDetail.java
@@ -1,35 +1,39 @@
-package org.group40fs1workingproject.security;
+package doctorBookingApp.security;
 
-import org.group40fs1workingproject.domain.Manager;
+import doctorBookingApp.entity.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
 
-public class ManagerUserDetails implements UserDetails {
+public class UserDetail implements UserDetails {
 
-    private final Manager manager;
+    private final User user;
 
-    public ManagerUserDetails(Manager manager) {
-        this.manager = manager;
+    public UserDetail(User user) {
+        this.user = user;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singletonList(new SimpleGrantedAuthority(manager.getRole().getAuthority()));
+        return Collections.singletonList(new SimpleGrantedAuthority(getAuthority()));
+    }
+
+    private String getAuthority() {
+
+        return "USER"; // Пример роли
     }
 
     @Override
     public String getPassword() {
-        return manager.getPassword();
+        return user.getHashPassword();
     }
 
     @Override
     public String getUsername() {
-        return manager.getManagerName();
+        return user.getEmail();
     }
 
     @Override
