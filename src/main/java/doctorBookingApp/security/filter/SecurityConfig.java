dd95abf5068fa50304daa22b0b9af76fa24223e2
JuliaPashkova <jjstudio1cn@gmail.com>
2024-08-02 05:53:39 +0200

    check security

--- src/main/java/doctorBookingApp/security/filter/SecurityConfig.java
+++ src/main/java/doctorBookingApp/security/filter/SecurityConfig.java
@@ -9,16 +9,15 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
 
 import java.util.List;
 
