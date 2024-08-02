d1627ae8f0b8b0af89bc2c7124f359316fc15c1f
NataliBurlakova <156796031+NataliBurlakova@users.noreply.github.com>
2024-08-02 19:21:16 +0300

    Update UserRepository.java

--- src/main/java/doctorBookingApp/repository/UserRepository.java
+++ src/main/java/doctorBookingApp/repository/UserRepository.java
@@ -8,9 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-/*
-Репозиторий определяет методы для поиска пользователей по различным полям и проверки их существования.
- */
+
 
 public interface UserRepository extends JpaRepository<User, Long>{
 
