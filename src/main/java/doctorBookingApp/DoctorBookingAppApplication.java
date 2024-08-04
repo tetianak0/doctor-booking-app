031afce4a0336f17ea38dde83f451c1f2788e428
JuliaPashkova <jjstudio1cn@gmail.com>
2024-08-04 10:08:27 +0200

    Update DoctorBookingAppApplication.java

--- src/main/java/doctorBookingApp/DoctorBookingAppApplication.java
+++ src/main/java/doctorBookingApp/DoctorBookingAppApplication.java
@@ -1,4 +1,4 @@
-package org.example.doctorbookingapp;
+package doctorBookingApp.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
