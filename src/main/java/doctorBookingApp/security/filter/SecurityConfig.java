ca7d4790df91b46e4a0a7000830ee6ddf6df6a14
JuliaPashkova <jjstudio1cn@gmail.com>
2024-08-05 21:18:51 +0200

    Update SecurityConfig.java

--- src/main/java/doctorBookingApp/security/filter/SecurityConfig.java
+++ src/main/java/doctorBookingApp/security/filter/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/hallo/**").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
 //                        .requestMatchers("/api/auth/**").permitAll()
 //                        .requestMatchers("/api/public/**").permitAll()
 //                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
