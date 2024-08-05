5e112879a46fce1186d442f57ceccc2b6ddb46bd
TetianaKolesnichenko <tetiana.kolesnichenko.de@gmail.com>
2024-08-05 12:32:25 +0200

    добавила AdminController,  в UserService добавила недостающие методы для реализации класса

--- src/main/java/doctorBookingApp/dto/TimeSlotDTO.java
+++ src/main/java/doctorBookingApp/dto/TimeSlotDTO.java
@@ -1,9 +1,12 @@
 package doctorBookingApp.dto;
+import doctorBookingApp.entity.TimeSlot;
 import doctorBookingApp.entity.enums.TypeOfInsurance;
 import lombok.*;
 
 @Getter
 @Setter
+@EqualsAndHashCode
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -13,4 +16,17 @@ public class TimeSlotDTO {
     private Long dateTime;
     private TypeOfInsurance insurance;
     private Boolean isBooked;
+
+    // Метод преобразования из сущности в DTO
+    public static TimeSlotDTO from(TimeSlot timeSlot) {
+        TimeSlotDTO dto = new TimeSlotDTO();
+        dto.setDoctorId(timeSlot.getId());
+        dto.setDateTime(timeSlot.getDateTime());
+        dto.setInsurance(timeSlot.getInsurance());
+        dto.setIsBooked(timeSlot.getIsBooked());
+        return dto;
+    }
+
+
+
 }
