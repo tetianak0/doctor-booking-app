78baad12b8693ce67461af8ddb2699db0ae5d907
NataliBurlakova <natali.burlakova.2022@gmail.com>
2024-08-05 23:24:10 +0300

    Update UserController. ПРоверила- приложение запускается

--- src/main/java/doctorBookingApp/controller/UserController.java
+++ src/main/java/doctorBookingApp/controller/UserController.java
@@ -1,112 +1,162 @@
-// package doctorBookingApp.controller;
+ package doctorBookingApp.controller;
 
-// import doctorBookingApp.controller.api.UserApi;
-// import doctorBookingApp.dto.NewUserDTO;
-// import doctorBookingApp.dto.UserDTO;
-// import doctorBookingApp.exeption.RestException;
-// import doctorBookingApp.service.ConfirmationCodeService;
-// import doctorBookingApp.service.UserService;
-// import jakarta.mail.MessagingException;
-// import lombok.RequiredArgsConstructor;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.*;
+ import doctorBookingApp.dto.NewUserDTO;
+ import doctorBookingApp.dto.StandardResponseDto;
+ import doctorBookingApp.dto.UserDTO;
+ import doctorBookingApp.exeption.RestException;
+ import doctorBookingApp.service.ConfirmationCodeService;
+ import doctorBookingApp.service.UserService;
+ import io.swagger.v3.oas.annotations.Operation;
+ import io.swagger.v3.oas.annotations.media.Content;
+ import io.swagger.v3.oas.annotations.media.Schema;
+ import io.swagger.v3.oas.annotations.responses.ApiResponse;
+ import io.swagger.v3.oas.annotations.responses.ApiResponses;
+ import jakarta.mail.MessagingException;
+ import lombok.RequiredArgsConstructor;
+ import org.springframework.http.HttpStatus;
+ import org.springframework.http.ResponseEntity;
+ import org.springframework.web.bind.annotation.*;
 
-// import java.util.List;
+ @RestController
+ @RequiredArgsConstructor
+ @RequestMapping("/users")  //все методы в этом классе будут доступны по URL, начинающимся с /users.
+ public class UserController {
 
-// @RestController
-// @RequiredArgsConstructor
-// public class UserController implements UserApi {
-
-//     private final UserService userService;
-//     private final ConfirmationCodeService confirmationCodeService;
-
-//     @PostMapping("/register")
-//     public ResponseEntity<String> registerUser(@RequestBody NewUserDTO newUser) throws MessagingException, RestException {
-//         userService.registrationUser(newUser);
-//         return ResponseEntity.ok("Регистрация практически закончена. Проверьте свой адрес электронной почты на наличие кода подтверждения");
-//     }
+     private final UserService userService;
+     private final ConfirmationCodeService confirmationCodeService;
 
 
-//     @PostMapping("/confirm")
-//     public ResponseEntity<?> confirmUser(@RequestParam String confirmationCode) {
-//         try {
-//             UserDTO userDTO = confirmationCodeService.confirmation(confirmationCode);
-//             return ResponseEntity.ok(userDTO);
-//         } catch (RestException e) {
-//             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Неверный код подтверждения");
-//         }
-//     }
+     //РЕГИСТРАЦИЯ ПОЛЬЗОВАТЕЛЯ
+
+     @PostMapping("/register")
+     public ResponseEntity<String> registerUser(@RequestBody NewUserDTO newUser) throws MessagingException, RestException {
+         userService.registrationUser(newUser);
+         return ResponseEntity.ok("Регистрация практически закончена. Проверьте свой адрес электронной почты на наличие кода подтверждения");
+     }
 
 
-//     @GetMapping("/{userId}")
-//     public ResponseEntity<UserDTO> getUserById(@PathVariable Long userId) throws RestException {
-//         return ResponseEntity.ok(userService.getUserById(userId));
-//     }
+     @PostMapping("/confirm")
+     public ResponseEntity<?> confirmUser(@RequestParam String confirmationCode) {
+         try {
+             UserDTO userDTO = confirmationCodeService.confirmation(confirmationCode);
+             return ResponseEntity.ok(userDTO);
+         } catch (RestException e) {
+             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Неверный код подтверждения");
+         }
+     }
 
 
-//     @PutMapping("/{userId}")
-//     public ResponseEntity<UserDTO> updateUser(@PathVariable Long userId, @RequestBody UserDTO userDTO) throws RestException {
-//         return ResponseEntity.ok(userService.editUser(userId, userDTO));
-//     }
+     //ПОЛУЧЕНИЕ ИНФОРМАЦИИ О ПОЛЬЗОВАТЕЛЕ
 
+     @Operation(summary = "Получение информации о пользователе по ID")
+     @ApiResponses(value = {
+             @ApiResponse(responseCode = "200", description = "Информация о пользователе",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserDTO.class))),
+             @ApiResponse(responseCode = "404", description = "Пользователь не найден.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = StandardResponseDto.class)))
 
-//     @Override
-//     public ResponseEntity<List<UserDTO>> getUsersByFullName(String surName, String firstName) {
-//         return ResponseEntity.ok(userService.getUserByFullName(firstName, surName));
-//     }
+     })
 
-//     @PutMapping("/{phoneNumber}")
-//     public ResponseEntity<UserDTO> getUserByPhoneNumber(String phoneNumber) throws RestException {
-//         return ResponseEntity.ok(userService.getUserByPhoneNumber(phoneNumber));
-//     }
-
-//     @PutMapping("/{email}")
-//     public ResponseEntity<UserDTO> getUserByEmail(String email) throws RestException {
-//         return ResponseEntity.ok(userService.getUserByEmail(email));
-//     }
-
-
-//     @Override
-//     public ResponseEntity<Void> deleteUserByPhoneNumber(String phoneNumber) throws RestException {
-//         userService.deleteUserByPhoneNumber(phoneNumber);
-//         return ResponseEntity.noContent().build();
-//     }
-
-//     @Override
-//     public ResponseEntity<Void> deleteUserByEmail(String email) throws RestException {
-//         userService.deleteUserByEmail(email);
-//         return ResponseEntity.noContent().build();
-//     }
+     @GetMapping("/{userId}")
+     public ResponseEntity<UserDTO> getUserById(@PathVariable Long userId) throws RestException {
+         return ResponseEntity.ok(userService.getUserById(userId));
+     }
 
 
 
-//     //ПОЧЕМУ ИДЕЯ СДЕЛАЛА ЭТО ПЕРЕОПРДЕЛЕНИЕ С return null - НЕПОНЯТНО
-//     @Override
-//     public ResponseEntity<String> confirmationUser(String confirmationCode) {
-//         return null;
-//     }
+     @Operation(summary = "Обновление информации о пользователе")
+     @ApiResponses(value = {
+             @ApiResponse(responseCode = "200", description = "Информация о пользователе обновлена.",
+                     content = @Content(mediaType = "application/json",
+                             schema = @Schema(implementation = UserDTO.class))),
+             @ApiResponse(responseCode = "404", description = "Пользователь не найден.",
+                     content = @Content(mediaType = "application/json",
+                             schema = @Schema(implementation = StandardResponseDto.class)))
 
-//     @Override
-//     public ResponseEntity<UserDTO> getUserById(long userID) {
-//         return null;
-//     }
+     })
 
-//     @Override
-//     public ResponseEntity<UserDTO> editUser(Long userID, UserDTO userDto) {
-//         return null;
-//     }
 
-//     @Override
-//     public ResponseEntity<List<UserDTO>> findAllUserById() {
-//         return null;
-//     }
+     @PutMapping("/{userId}")
+     public ResponseEntity<UserDTO> updateUser(@PathVariable Long userId, @RequestBody UserDTO userDTO) throws RestException {
+         return ResponseEntity.ok(userService.editUser(userId, userDTO));
+     }
 
-//     @Override
-//     public ResponseEntity<UserDTO> deleteUserById(long userID) {
-//         return null;
-//     }
-// }
+
+     @Operation(summary = "Получение информации о пользователе по email")
+     @ApiResponses(value = {
+             @ApiResponse(responseCode = "200", description = "Информация о пользователе",
+                     content = @Content(mediaType = "application/json",
+                             schema = @Schema(implementation = UserDTO.class))),
+             @ApiResponse(responseCode = "404", description = "Пользователь не найден.",
+                     content = @Content(mediaType = "application/json",
+                             schema = @Schema(implementation = StandardResponseDto.class)))
+
+     })
+
+     @PutMapping("/{email}")
+     public ResponseEntity<UserDTO> getUserByEmail(@PathVariable String email) throws RestException {
+         return ResponseEntity.ok(userService.getUserByEmail(email));
+     }
+
+
+
+     @Operation(summary = "Получение информации о пользователе по номеру телефона")
+     @ApiResponses(value = {
+             @ApiResponse(responseCode = "200", description = "Информация о пользователе",
+                     content = @Content(mediaType = "application/json",
+                             schema = @Schema(implementation = UserDTO.class))),
+             @ApiResponse(responseCode = "404", description = "Пользователь не найден.",
+                     content = @Content(mediaType = "application/json",
+                             schema = @Schema(implementation = StandardResponseDto.class)))
+
+     })
+
+     @PutMapping("/{phoneNumber}")
+     public ResponseEntity<UserDTO> getUserByPhoneNumber(@PathVariable String phoneNumber) throws RestException {
+         return ResponseEntity.ok(userService.getUserByPhoneNumber(phoneNumber));
+     }
+
+
+        //УДАЛЕНИЕ ПОЛЬЗОВАТЕЛЯ
+
+    @Operation(summary = "Удаление пользователя по email")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Пользователь удален."),
+            @ApiResponse(responseCode = "404", description = "Пользователь не найден.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = StandardResponseDto.class)))
+    })
+
+     @DeleteMapping("/by-email/{email}")
+     public ResponseEntity<Void> deleteUserByEmail(@PathVariable String email) throws RestException {
+         userService.deleteUserByEmail(email);
+         return ResponseEntity.noContent().build();
+     }
+
+
+
+     @Operation(summary = "Удаление пользователя по номеру телефона")
+     @ApiResponses(value = {
+             @ApiResponse(responseCode = "204", description = "Пользователь удален."),
+             @ApiResponse(responseCode = "404", description = "Пользователь не найден.",
+                     content = @Content(mediaType = "application/json",
+                             schema = @Schema(implementation = StandardResponseDto.class)))
+     })
+
+     @DeleteMapping("/by-phone/{phoneNumber}")
+     public ResponseEntity<Void> deleteUserByPhoneNumber(@PathVariable String phoneNumber) throws RestException {
+         userService.deleteUserByPhoneNumber(phoneNumber);
+         return ResponseEntity.noContent().build();
+     }
+
+
+
+
+
+
+ }
 
 
 
