e56d0e3b9bee64443af8c9b8995cc232b1599337
JuliaPashkova <jjstudio1cn@gmail.com>
2024-08-06 08:09:05 +0200

    Update DepartmentController.java

--- src/main/java/doctorBookingApp/controller/DepartmentController.java
+++ src/main/java/doctorBookingApp/controller/DepartmentController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/departments")
+@RequestMapping("/departments")
 public class DepartmentController {
 
     private final DepartmentService departmentService;
